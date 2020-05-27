c0534d1a9df0a31d08e59587e886a03147293881
tyler broadway <tbroadway96@gmail.com>
2020-05-26 22:29:14 -0500

    Project complete

---
+++ migrations/20200526220009_cars-schema.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments('id');
+        tbl.string('vin', 128).unique().notNullable();
+        tbl.string('make', 128).notNullable();
+        tbl.string('model', 128).notNullable();
+        tbl.string('mileage', 128).notNullable();
+        tbl.string('transmissionType', 128);
+        tbl.string('titleStatus', 128);
+    });
+}
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars');
+};
